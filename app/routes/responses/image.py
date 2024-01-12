7ccd96280f789cd90d8508f79d0788b2e7b8f0ff
Felipe de Luca Andrea <71850278+fdelu@users.noreply.github.com>
2024-01-11 22:44:35 -0300

    Update app/routes/responses/image.py

--- app/routes/responses/image.py
+++ app/routes/responses/image.py
@@ -7,4 +7,4 @@ IMAGE_NOT_FOUND_ERROR = HTTPException(status.HTTP_404_NOT_FOUND, "Image not foun
 
 # This error is not actually used in the code, it's just for route documentation.
 # We can't use both IMAGE_NOT_FOUND_ERROR and USER_NOT_FOUND_ERROR for 404s in the docs.
-NOT_FOUND_ERROR = HTTPException(status.HTTP_404_NOT_FOUND, "User or image not found")
+NOT_FOUND_ERROR = HTTPException(status.HTTP_404_NOT_FOUND, "Store or image not found")
