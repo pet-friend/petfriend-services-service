f6a69df1c4b6d795ace573da29c4f6a5497069f7
Gabrielsem <gsemorile@fi.uba.ar>
2024-02-24 22:00:20 -0300

    add shipping cost

--- tests/fixtures/stores.py
+++ tests/fixtures/stores.py
@@ -4,16 +4,19 @@ valid_store = {
     "name": "test store",
     "description": "test",
     "delivery_range_km": 5,
+    "shipping_cost": 10,
 }
 
 valid_store2 = {
     "name": "test store2",
     "description": "test2",
     "delivery_range_km": 5,
+    "shipping_cost": 10,
 }
 
 invalid_store = {
     "name": "test",
     "description": "test",
     "delivery_range_km": -1,
+    "shipping_cost": -1,
 }
